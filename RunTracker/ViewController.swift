1a3891aa6c90ba03441fb5b1eef97354bd777118
drewcappel <cappeldrew@gmail.com>
2019-07-31 21:30:04 -0400

    Initial Commit

--- RunTracker/ViewController.swift
+++ RunTracker/ViewController.swift
@@ -18,3 +18,14 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+}
